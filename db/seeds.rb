bde7e3ce8c8677a703e9f8f7bca459e091ad39b6
Najma <najmathande@gmail.com>
2023-07-02 00:56:50 +0300

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-la = Gym.create(name: "L.A. Fitness", address: "Suite 981 1795 Leida Fords, Nataliemouth, TN 14377-4979")
-planet = Gym.create(name: "Planet Fitness", address: "7768 David Avenue, Greenside, PA 67475-3655")
+# Create Gyms
+Gym.create(name: 'L.A.Fitness', address: 'Suite 981 1795 Leida Fords, Nataliemouth, TN 14377-4979')
+Gym.create(name: 'Planet Fitness', address: '7768 David Avenue, Greenside, PA 67475-3655')
 
-cassie = Client.create(name: "Casie Effertz", age: 18)
-dorris = Client.create(name: "Dorris Bogan", age: 52)
-bernie = Client.create(name: "Bernie Schiller", age: 44)
-manie = Client.create(name: "Manie Dare", age: 29)
+# Create Clients
+Client.create(name: 'Casie Effertz', age: 18)
+Client.create(name: 'Dorris Bogan', age: 52)
+Client.create(name: 'Bernie Schiller', age: 44)
+Client.create(name: 'Manie Dare', age: 29)
 
-Membership.create(gym_id: la.id, client_id: cassie.id, charge: 40)
-Membership.create(gym_id: la.id, client_id: dorris.id, charge: 40)
-Membership.create(gym_id: la.id, client_id: bernie.id, charge: 40)
-Membership.create(gym_id: planet.id, client_id: cassie.id, charge: 50)
-Membership.create(gym_id: planet.id, client_id: manie.id, charge: 60)
+# Create Memberships
+Membership.create(gym_id: 1, client_id: 1, charge: 40)
+Membership.create(gym_id: 1, client_id: 2, charge: 40)
+Membership.create(gym_id: 2, client_id: 1, charge: 50)
+Membership.create(gym_id: 2, client_id: 3, charge: 60)
